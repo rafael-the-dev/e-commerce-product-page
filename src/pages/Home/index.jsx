4d13d9546a4d6d001e8a5e47b834485c81a725c3
Rafael Tivane <luislangabusiness@gmail.com>
2021-12-08 13:32:51 +0200

    adding content to hero section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,7 +2,7 @@ import Header from '../../components/Header';
 import ImageGallery from 'react-image-gallery';
 import { useRef } from 'react';
 import classNames from 'classnames';
-import { useDisplay } from '../../styles';
+import { useDisplay, useTypography } from '../../styles';
 import { useStyles } from './styles';
 import imageProduct1 from '../../assets/images/image-product-1.jpg'
 import imageProduct1Thumbnail from '../../assets/images/image-product-1-thumbnail.jpg'
@@ -12,10 +12,12 @@ import imageProduct3 from '../../assets/images/image-product-3.jpg'
 import imageProduct3Thumbnail from '../../assets/images/image-product-3-thumbnail.jpg'
 import imageProduct4 from '../../assets/images/image-product-4.jpg'
 import imageProduct4Thumbnail from '../../assets/images/image-product-4-thumbnail.jpg'
+import { Typography } from '@mui/material';
 
 const Home = () => {
 
     const display = useDisplay();
+    const text = useTypography();
     const classes = useStyles();
 
     const images = useRef([
@@ -44,6 +46,23 @@ const Home = () => {
                 <section className={classNames(display.flex, display.flexColumn, classes.heroSection)}>
                     <div className={classNames(classes.heroSectionImageGalleryContainer)}>
                         <ImageGallery showThumbnails={false} items={images.current} />
+                    </div>
+                    <div className={classNames(classes.heroSectionContent)}>
+                        <Typography component="h2" variant="h5" className={classNames(classes.heroSectionTitle, text.font7)}>
+                            Fall Limited Edition<br />Sneackers
+                        </Typography>
+                        <Typography className={classNames(display.mt1, display.opacity9, classes.heroSectionDescription)}>
+                            These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.
+                        </Typography>
+                        <div className={classNames(display.mt2, display.flex, display.alignCenter, display.justifyBetween)}>
+                            <div className={classNames(display.flex, display.alignCenter)}>
+                                <Typography component="h3" variant="h5" className={classNames(classes.heroSectionPrice, text.font7)}>
+                                    $125.00
+                                </Typography>
+                                <span className={classNames(classes.heroSectionDiscount, display.ml1, text.font7)}>50%</span>
+                            </div>
+                            <span className={classNames(classes.heroSectionWeiredPrice, text.font7)}>$250.00</span>
+                        </div>
                     </div>
                 </section>
             </main>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,4 +2,35 @@ import { makeStyles } from "@mui/styles";
 
 
 export const useStyles = makeStyles(theme => ({
+    heroSectionContent: {
+        padding: '2rem 5%'
+    },
+    heroSectionTitle: {
+        '&::before': {
+            color: '#FF7E1B',
+            content: '"Sneaker Company"',
+            display: 'block',
+            fontSize: '1.1rem',
+            fontWeight: '500 !important',
+            marginBottom: '1rem',
+            marginLeft: 'auto',
+
+        }
+    },
+    heroSectionDescription: {
+        color: '#69707D'
+    },
+    heroSectionPrice: {
+        fontSize: '1.2rem !important'
+    },
+    heroSectionDiscount: {
+        backgroundColor: '#FFEEE2',
+        borderRadius: 7,
+        color: '#FF7E1B',
+        padding: '0.4rem 0.7rem',
+    },
+    heroSectionWeiredPrice: {
+        color: '#B6BCC8',
+        textDecoration: 'line-through'
+    }
 }))
