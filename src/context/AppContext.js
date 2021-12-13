c399780359439cf019425693b072412405d41dc1
Rafael Tivane <luislangabusiness@gmail.com>
2021-12-13 22:06:04 +0200

    keep cart items at localStorage and getting them back after page refresh

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback, useState, useRef } from 'react';
+import React, { createContext, useCallback, useState, useRef, useEffect } from 'react';
 import image1 from '../assets/images/image-product-1.jpg'
 
 export const AppContext = createContext();
@@ -25,22 +25,40 @@ export const AppContextProvider = ({ children }) => {
     ]);
 
     const getProducts = useCallback(() => [ ...products.current ], []);
+    const localStorageName = useRef('e-commerce-product-page');
+    const addItemsToLocalStorage = useCallback(items => {
+        localStorage.setItem(localStorageName.current, JSON.stringify(items));
+    }, [])
 
     const addProductToCart = useCallback((id, quantity) => {
         const product = getProducts().find(item => item.id = id);
         if(product) {
             setCartList(oldList => {
-                return [ ...oldList, { id: oldList.length + 1, product, quantity, totalPrice: product.price * quantity }]
+                const items =  [ ...oldList, { id: oldList.length + 1, product, quantity, totalPrice: product.price * quantity }];
+                addItemsToLocalStorage(items);
+                return items;
             });
         }
-    }, [ getProducts ]);
+    }, [ addItemsToLocalStorage, getProducts ]);
 
     const removeProductFromCart = useCallback((id) => () => {
         const product = getProducts().find(item => item.id = id);
         if(product) {
-            setCartList(oldList => oldList.filter(item => item.id !== id));
+            setCartList(oldList => {
+                const items = oldList.filter(item => item.id !== id);
+                addItemsToLocalStorage(items);
+                return items;
+            });
         }
-    }, [ getProducts ]);
+    }, [ addItemsToLocalStorage, getProducts ]);
+
+    useEffect(() => {
+        const items = JSON.parse(localStorage.getItem(localStorageName.current));
+        console.log(Array.isArray(items))
+        if(Array.isArray(items)) {
+            setCartList(items);
+        }
+    }, [])
 
     return (
         <AppContext.Provider value={{ addProductToCart, closeCartDialog, currentPage, getProducts, getCartList, openCartDialog, 
